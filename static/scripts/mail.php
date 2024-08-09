82f852e7734de7bee3e6ebacf31eb031e3df55d3
Dani <96098717+ALifeLivedFully@users.noreply.github.com>
2024-08-09 22:05:30 +0000

    Update mail.php

--- static/scripts/mail.php
+++ static/scripts/mail.php
@@ -1,20 +1,19 @@
 <?php
 
 $name = $_POST['name'];
-$phone = $_POST['phone'];
+// $phone = $_POST['phone'];
 $email = $_POST['email'];
-$projecttype = $_POST['projectType'];
+// $projecttype = $_POST['projectType'];
 $aboutproject = $_POST['aboutProject'];
 
-$header = "From:".$name."<".$email.">\r\n";
+// $header = "From:".$name."<".$email.">\r\n";
 $recipient = "info@alifelivedfully.com";
 $subject = "New contact form submission";
 // $message = "Name: ".$name."Phone: ".$phone."Email: ".$email."Project Type: ".$projecttype."About The Project: ".$aboutproject."";
 $message = "This is a test";
 
-mail($recipient,$subject,$message,$header)
-or die("Error!");
-
-echo "Message sent";
+mail($recipient,$subject,$message)
+  echo "Message sent"
+  or die("Error!");
 
 ?>
