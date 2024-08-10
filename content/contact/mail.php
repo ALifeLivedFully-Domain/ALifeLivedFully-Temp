16fec4ad35b16d02092ea016a5aa780bf6197c3a
Dani <Dani@ALifeLivedFully.com>
2024-08-09 19:22:22 -0500

    remove die from mailer

--- content/contact/mail.php
+++ content/contact/mail.php
@@ -18,8 +18,7 @@ $to = "info@alifelivedfully.com";
 $subject = "New contact form submission";
 $message = "Name: " . "\r\n" . $name . "\r\n" . "\r\n" . "Phone: " . "\r\n" . $phone . "\r\n" . "\r\n" . "Email: " . "\r\n" . $email . "\r\n" . "\r\n" . "Project Type: " . "\r\n" . $projecttype . "\r\n" . "\r\n" . "About The Project: " . "\r\n" . $aboutproject . "\r\n" . "\r\n";
 
-mail($to, $subject, $message, $headers)
-  or die("Error!");
+mail($to, $subject, $message, $headers);
 
 echo <h1> Mail.php </h1>
 ?>
