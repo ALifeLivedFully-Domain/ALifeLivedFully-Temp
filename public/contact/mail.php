d385a3a04bd32f97cad306dcf91b88867491f295
Dani <Dani@ALifeLivedFully.com>
2024-08-10 21:33:12 -0500

    user validation and redirect

--- public/contact/mail.php
+++ public/contact/mail.php
@@ -1,16 +1,22 @@
 <?php
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$projecttype = $_POST['project-type'];
-$aboutproject = $_POST['about-project'];
+header('Location: https://alifelivedfully.com');
+$message_sent = false;
+if (isset($_POST['email'])) {
+    if (filter_var($_POST['email']), FILTER_VALIDATE_EMAIL) {
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $projecttype = $_POST['project-type'];
+        $aboutproject = $_POST['about-project'];
 
-$to = "info@alifelivedfully.com";
-$subject = "New contact form submission";
-$message = "Name: " . "\r\n" . $name . "\r\n" . "\r\n" . "Phone: " . "\r\n" . $phone . "\r\n" . "\r\n" . "Email: " . "\r\n" . $email . "\r\n" . "\r\n" . "Project Type: " . "\r\n" . $projecttype . "\r\n" . "\r\n" . "About The Project: " . "\r\n" . $aboutproject . "\r\n" . "\r\n";
-$headers = "From: webmaster@alifelivedfully.com" . "\r\n" .
-    "Reply-To: " . $email . "\r\n";
+        $to = "dani@alifelivedfully.com";
+        $subject = "New contact form submission";
+        $message = "Name: " . "\r\n" . $name . "\r\n" . "\r\n" . "Phone: " . "\r\n" . $phone . "\r\n" . "\r\n" . "Email: " . "\r\n" . $email . "\r\n" . "\r\n" . "Project Type: " . "\r\n" . $projecttype . "\r\n" . "\r\n" . "About The Project: " . "\r\n" . $aboutproject . "\r\n" . "\r\n";
+        $headers = "From: " . $email . "\r\n" .
+                   "Reply-To: " . $email . "\r\n";
 
-mail($to, $subject, $message, $headers);
-echo "<p>Message Sent</p>";
+        mail($to, $subject, $message, $headers);
+        $message_sent = true;
+    }
+}
 ?>
