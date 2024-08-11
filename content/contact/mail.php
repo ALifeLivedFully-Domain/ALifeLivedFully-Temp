346f37a2b31242ec30c1b06e31cc370fef007edc
Dani <Dani@ALifeLivedFully.com>
2024-08-11 13:54:51 -0500

    sanitize and validate user input

--- content/contact/mail.php
+++ content/contact/mail.php
@@ -1,22 +1,38 @@
 <?php
+
+// set message sent to false by defailt
 $message_sent = false;
-if (isset($_POST['email'])) {
-    if (filter_var($_POST['email']), FILTER_VALIDATE_EMAIL) {
-        $name = $_POST['name'];
-        $phone = $_POST['phone'];
-        $email = $_POST['email'];
-        $projecttype = $_POST['project-type'];
-        $aboutproject = $_POST['about-project'];
 
-        $to = "dani@alifelivedfully.com";
-        $subject = "New contact form submission";
-        $message = "Name: " . "\r\n" . $name . "\r\n" . "\r\n" . "Phone: " . "\r\n" . $phone . "\r\n" . "\r\n" . "Email: " . "\r\n" . $email . "\r\n" . "\r\n" . "Project Type: " . "\r\n" . $projecttype . "\r\n" . "\r\n" . "About The Project: " . "\r\n" . $aboutproject . "\r\n" . "\r\n";
-        $headers = "From: " . $email . "\r\n" .
-                   "Reply-To: " . $email . "\r\n";
+// Validate user has submitted form
+if (isset($_POST['submit'])) {
 
-        mail($to, $subject, $message, $headers);
-        $message_sent = true;
-    }
+    // Sanitize user input
+    $name           = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+    $phone          = filter_input(INPUT_POST, "phone", FILTER_SANITIZE_NUMBER_INT); 
+    $email          = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $projecttype    = filter_input(INPUT_POST, "projecttype", FILTER_SANITIZE_SPECIAL_CHARS);
+    $aboutproject   = filter_input(INPUT_POST, "aboutproject", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    // Set variables for mail function
+    $from       =   $email;
+
+    $to         =   "Dani@alifelivedfully.com";
+
+    $subject    =   "Requesting quote for project type: " . $projecttype;
+
+    $headers    =   "From: " . $email . "\r\n" .
+                    "Reply-To: " . $email . "\r\n";
+
+    $message    =   "Name: " . "\r\n" . $name . "\r\n" . "\r\n" . 
+                    "Phone: " . "\r\n" . $phone . "\r\n" . "\r\n" . 
+                    "Email: " . "\r\n" . $email . "\r\n" . "\r\n" . 
+                    "Project Type: " . "\r\n" . $projecttype . "\r\n" . "\r\n" . 
+                    "About The Project: " . "\r\n" . $aboutproject . "\r\n" . "\r\n";
+
+    // Send the email using mail function(built into php by default)
+    mail($to, $subject, $message, $headers);
+
+    // Update message sent var
+    $message_sent = true;
 }
-header('Location: https://alifelivedfully.com');
 ?>
